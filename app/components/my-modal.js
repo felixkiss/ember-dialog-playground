c17c39070bf3a402559a1db723554f8d356c5c92
Michael Pitzer <mail@mpitzer.at>
2016-12-03 01:51:34 +0100

    Nuke scrolling of body when modal is open

--- app/components/my-modal.js
+++ app/components/my-modal.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   show: false,
 
+  _onVisibilityChange: Ember.observer('show', function() {
+    Ember.$('body').toggleClass('modal-open', this.get('show'));
+  }),
+
   noop: Ember.K,
 
   close() {
